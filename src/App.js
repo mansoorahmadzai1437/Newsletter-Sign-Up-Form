6f6dc787170b1d394686ee09b3198bfb6575d4e1
mansoorahmadzai1437 <mansoorahmadzai1437@gmail.com>
2024-10-10 18:29:37 +0430

    Add mobile static version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,76 @@
+import { useState } from "react";
+
 export default function App() {
+  const [email, setEmail] = useState("");
+
+  function handleEmailValueChange(e) {
+    setEmail(e.target.value);
+  }
+
   return (
-    <h1 className="p-12 text-3xl">
-      Welcome to the Newsletter Sign-Up Form Mini Project!
-    </h1>
+    // Background Container
+    <div className="bg-DarkSlateGrey font-Roboto">
+      {/* Card Container */}
+      <div className="flex flex-col bg-White">
+        {/* Image */}
+        <img
+          src="/illustration-sign-up-mobile.svg"
+          alt="Illustration"
+          className="w-full rounded-b-md"
+        />
+
+        {/* Content */}
+        <div className="px-6 flex flex-col space-y-7 py-9">
+          {/* Title */}
+          <h1 className="text-[40px] font-bold text-DarkSlateGrey">
+            Stay updated!
+          </h1>
+
+          {/* Paragraph */}
+          <p className="text-DarkSlateGrey">
+            Join 60,000+ product managers receiving monthly updates on:
+          </p>
+
+          {/* List */}
+          <div className="flex flex-col space-y-3">
+            <ListItem>Product discovery and building what matters</ListItem>
+            <ListItem>Measuring to ensure updates are a success</ListItem>
+            <ListItem>And much more!</ListItem>
+          </div>
+
+          {/* Email Input */}
+          <form className="flex flex-col space-y-6 pt-3">
+            <div className="flex flex-col space-y-3">
+              <label className="text-sm font-bold">Email address</label>
+              <input
+                value={email}
+                onChange={handleEmailValueChange}
+                type="text"
+                className="border border-Grey p-4 px-6 rounded-lg active:border-CharcoalGrey"
+                placeholder="email@company.com"
+              />
+            </div>
+            <Button>Subscribe to monthly newsletter</Button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ListItem({ children }) {
+  return (
+    <div className="flex space-x-5 items-start">
+      <img src="icon-list.svg" alt="icon" className="w-6" />
+      <p className="text-DarkSlateGrey">{children}</p>
+    </div>
+  );
+}
+
+function Button({ children }) {
+  return (
+    <button className="bg-DarkSlateGrey text-White w-full p-4 rounded-lg hover:bg-gradient-to-r hover:from-rose-400 hover:via-Tomato hover:to-Tomato hover:shadow-ButtonShadow">
+      {children}
+    </button>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Primary
+        Tomato: "hsl(4, 100%, 67%)",
+
+        // Neutral
+        DarkSlateGrey: "hsl(234, 29%, 20%)",
+        CharcoalGrey: "hsl(235, 18%, 26%)",
+        Grey: "hsl(231, 7%, 60%)",
+        White: "hsl(0, 0%, 100%)",
+      },
+
+      fontFamily: {
+        Roboto: ["Roboto", "sans"],
+      },
+      boxShadow: {
+        ButtonShadow: "0 20px 60px -15px hsl(4, 100%, 67%)",
+      },
+    },
   },
   plugins: [],
 };
