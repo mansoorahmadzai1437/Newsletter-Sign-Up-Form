abf44bd3e8f591a414126804b91b5128cea0f87f
mansoorahmadzai1437 <mansoorahmadzai1437@gmail.com>
2024-10-11 17:48:06 +0430

    Add desktop version

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ export default function App() {
 
   return (
     // Background Container
-    <div className="bg-DarkSlateGrey font-Roboto">
+    <div className="bg-CharcoalGrey font-Roboto md:flex md:flex-col md:justify-center md:items-center md:h-screen">
       {/* Card Container */}
       {view === "form" ? (
         <FormView
@@ -45,23 +45,29 @@ export default function App() {
 
 function FormView({ onFormSubmit, email, onWriteEmail, wrongEmail }) {
   return (
-    <div className="flex flex-col bg-White">
+    <div className="flex flex-col bg-White md:flex-row-reverse md:p-4 md:rounded-3xl">
       {/* Image */}
       <img
         src="/illustration-sign-up-mobile.svg"
         alt="Illustration"
-        className="w-full"
+        className="w-full md:hidden"
+      />
+
+      <img
+        src="/illustration-sign-up-desktop.svg"
+        alt="Illustration"
+        className="hidden md:block"
       />
 
       {/* Content */}
-      <div className="px-6 flex flex-col space-y-7 py-10">
+      <div className="px-6 flex flex-col space-y-7 py-10 md:justify-center md:px-12 md:space-y-4">
         {/* Title */}
-        <h1 className="text-[40px] font-bold text-DarkSlateGrey">
+        <h1 className="text-[40px] md:text-[3.5rem] font-bold text-DarkSlateGrey">
           Stay updated!
         </h1>
 
         {/* Paragraph */}
-        <p className="text-DarkSlateGrey">
+        <p className="text-DarkSlateGrey md:w-96">
           Join 60,000+ product managers receiving monthly updates on:
         </p>
 
@@ -76,7 +82,7 @@ function FormView({ onFormSubmit, email, onWriteEmail, wrongEmail }) {
         <form className="flex flex-col space-y-6 pt-3" onSubmit={onFormSubmit}>
           <div className="flex flex-col space-y-3">
             <div className="flex justify-between items-center">
-              <label className="text-sm font-bold">Email address</label>
+              <label className="text-xs font-bold">Email address</label>
               {wrongEmail && <ErrorMessage />}
             </div>
             <input
@@ -87,7 +93,8 @@ function FormView({ onFormSubmit, email, onWriteEmail, wrongEmail }) {
                 wrongEmail
                   ? "border-Tomato bg-Tomato bg-opacity-15 placeholder:text-Tomato text-Tomato"
                   : "border-Grey text-DarkSlateGrey"
-              } border p-4 px-6 rounded-lg active:border-CharcoalGrey`}
+              } border p-4 px-6 rounded-lg active:border-CharcoalGrey focus:outline-DarkSlateGrey
+              active:outline-DarkSlateGrey cursor-pointer`}
               placeholder="email@company.com"
             />
           </div>
@@ -100,17 +107,21 @@ function FormView({ onFormSubmit, email, onWriteEmail, wrongEmail }) {
 
 function SubmittedView({ email, onDismiss }) {
   return (
-    <div className="bg-White h-screen px-6">
+    <div className="bg-White h-screen px-6 md:h-auto md:rounded-3xl md:p-14 md:w-[31rem]">
       {/* Content */}
-      <div className="flex flex-col space-y-8 pt-40">
+      <div className="flex flex-col space-y-8 pt-40 md:pt-0 md:space-y-5">
         {/* Icon */}
-        <img src="icon-success.svg" alt="Icon" className="w-[4.5rem]" />
+        <img
+          src="icon-success.svg"
+          alt="Icon"
+          className="w-[4.5rem] md:w-[4rem]"
+        />
         {/* Title */}
-        <h1 className="text-DarkSlateGrey font-bold text-[2.65rem] leading-none">
+        <h1 className="text-DarkSlateGrey font-bold text-[2.65rem] md:text-[3.5rem] leading-none">
           Thanks for subscribing!
         </h1>
         {/* Paragraph */}
-        <p>
+        <p className="text-DarkSlateGrey">
           A confirmation email has been sent to{" "}
           <span className="font-bold">{email}</span>. Please open it and click
           the button inside to confirm your subscription.
@@ -119,7 +130,11 @@ function SubmittedView({ email, onDismiss }) {
 
       {/* Dismiss Button */}
       <form onSubmit={onDismiss}>
-        <Button classes={"fixed bottom-10 right-6 left-6 w-auto "}>
+        <Button
+          classes={
+            "fixed bottom-10 right-6 left-6 md:static md:mt-10 md:w-full"
+          }
+        >
           Dismiss message
         </Button>
       </form>
@@ -139,7 +154,7 @@ function ListItem({ children }) {
 function Button({ children, classes }) {
   return (
     <button
-      className={`${classes} bg-DarkSlateGrey text-White w-full p-4 rounded-lg hover:bg-gradient-to-r md:hover:from-rose-400 md:hover:via-Tomato md:hover:to-Tomato md:hover:shadow-ButtonShadow active:bg-gradient-to-r active:from-rose-400 active:via-Tomato active:to-Tomato active:shadow-ButtonShadow`}
+      className={`bg-DarkSlateGrey text-White p-4 rounded-lg hover:bg-gradient-to-r md:hover:from-rose-400 md:hover:via-Tomato md:hover:to-Tomato md:hover:shadow-ButtonShadow active:bg-gradient-to-r active:from-rose-400 active:via-Tomato active:to-Tomato active:shadow-ButtonShadow ${classes}`}
     >
       {children}
     </button>
@@ -148,7 +163,7 @@ function Button({ children, classes }) {
 
 function ErrorMessage({ classes }) {
   return (
-    <span className="classes font-bold text-sm text-Tomato">
+    <span className="classes font-bold text-xs text-Tomato">
       Valid email required
     </span>
   );
